2ac4df6adf8fb4887f36497a5e2f745b01a3018b
Mashima <52222646+zimashima@users.noreply.github.com>
2020-01-21 17:47:09 -0500

    stretch

--- car/carsRouter.js
+++ car/carsRouter.js
@@ -6,9 +6,51 @@ const router = express.Router()
 router.get("/", async (req, res, next) => {
     try{
         const cars = await db("cars").select()
-        res.json(cars)
+        res.status(200).json(cars)
     }
     catch (err) {
+        next(err)
+    }
+})
+
+
+router.get("/:id", async (req,res,next) => {
+    try {
+        const car = await db('cars').where( "id", req.params.id).first()
+        res.status(200).json(car)
+    } 
+    catch (err){
+        next(err)
+    }
+})
+
+
+router.post("/", async (req,res,next) => {
+    try{
+        const [id] = await db('cars').insert(req.body)
+        res.status(200).json(await db("cars").where("id", id).first())
+    }
+    catch (err){
+        next(err)
+    }
+})
+
+router.delete("/:id", async (req,res,next) => {
+    try{
+        await db('cars').where("id", req.params.id).del()
+        res.status(204).end()
+    }
+    catch (err){
+        next(err)
+    }
+})
+
+router.put("/:id", async (req,res,next) => {
+    try{
+        await db('cars').where("id", req.params.id).update(req.body)
+        res.status(200).json(await db("cars").where("id", req.params.id).first())
+    }
+    catch (err){
         next(err)
     }
 })
